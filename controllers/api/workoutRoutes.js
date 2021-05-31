1ea7f04ffdcf496cf3dbad4a7309e78f2775ae7a
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-05-31 10:27:42 -0500

    add method to add exercise on workout route

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -13,12 +13,24 @@ router.get("/", (req, res) => {
 
 router.post("/", async (req, res)=> {
     try{
-        const response = await db.Workout.create({type: "workout"})
+        const response = await db.Workout.create({})
         res.json(response);
     }
     catch(err){
         console.log("error occurred creating a workout: ", err)
     }
 })
+
+//adds new exercise
+router.put("/:id", ({ body, params }, res) => {
+
+    db.Workout.findByIdAndUpdate(params.id, { $push: { exercises: body } })
+        .then((updateWorkout) => {
+            res.json(updateWorkout);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
 
 module.exports = router;
