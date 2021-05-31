b205a392806ddfc0607d0ee7f6f17047a7274f48
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-05-31 08:01:22 -0500

    add controllers for getting pages

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const logger = require('morgan');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const routes = require("./controllers");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger('dev'));
 
 app.use(express.static("public"));
+app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
